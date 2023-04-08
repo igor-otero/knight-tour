6dc64e13f46914554b96bd7ea27dc37a62d72178
Igor Otero <igor.otero@gmail.com>
2023-04-08 20:58:41 +0200

    Parameter BOARD_SIZE and removal of some commented code.

--- knighttour/src/main/java/com/oterors/App.java
+++ knighttour/src/main/java/com/oterors/App.java
@@ -1,34 +1,17 @@
 package com.oterors;
-/*
-import java.awt.Point;
-import java.util.ArrayList;
-*/
-/**
- * Hello world!
- *
- */
-public class App 
-{
+
+public class App {
     public static void main( String[] args ) {
-/*
-        Board b = new Board(4);
-        b.set(1, 1);
-        System.out.println(b.isFull());
-        ArrayList<Point> moves = b.getMoves();
-        for (Point point : moves) {
-            System.out.println(point.toString());
-        }
-*/
-        Board b = new Board(8);
-        System.out.println("Resultado passeio 8 x 8: " + b.knightTour(0, 0));
+        final int BOARD_SIZE = 8;
+        Board b = new Board(BOARD_SIZE);
+        System.out.println("Resultado passeio " + BOARD_SIZE + " x " + BOARD_SIZE + ": " + b.knightTour(0, 0));
       
         int[][] maxBoard = b.getMaxBoard();
-        for (int i = 7; i >= 0; i--) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = BOARD_SIZE - 1; i >= 0; i--) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 System.out.printf("%02d ", maxBoard[i][j]);
             }
             System.out.println();
         }
-
     }
 }
